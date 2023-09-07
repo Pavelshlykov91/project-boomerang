7880fd7f0fa5896b1ba6e6f24a7cc86a51895750
Pavelshlykov91 <pavelshlykov91@gmail.com>
2023-09-07 18:55:23 +0300

    Boomerang back

--- src/Game.js
+++ src/Game.js
@@ -2,10 +2,10 @@
 // Или можно не импортировать,
 // а передавать все нужные объекты прямо из run.js при инициализации new Game().
 
-const Hero = require('./game-models/Hero');
-const Enemy = require('./game-models/Enemy');
-// const Boomerang = require('./game-models/Boomerang');
-const View = require('./View');
+const Hero = require("./game-models/Hero");
+const Enemy = require("./game-models/Enemy");
+const Boomerang = require("./game-models/Boomerang");
+const View = require("./View");
 
 // Основной класс игры.
 // Тут будут все настройки, проверки, запуск.
@@ -13,34 +13,48 @@ const View = require('./View');
 class Game {
   constructor({ trackLength }) {
     this.trackLength = trackLength;
-    this.hero = new Hero({position: 0}); // Герою можно аргументом передать бумеранг.
-    this.enemy = new Enemy({position: 30});
+    this.hero = new Hero({ position: 0 }); // Герою можно аргументом передать бумеранг.
+    this.enemy = new Enemy({ position: 30 });
+    this.boomerang = new Boomerang({ position: 1 });
     this.view = new View(this);
     this.track = [];
-    this.regenerateTrack(70);
+    // this.regenerateTrack(1000);
   }
 
   regenerateTrack() {
     // Сборка всего необходимого (герой, враг(и), оружие)
     // в единую структуру данных
-    this.track = (new Array(this.trackLength)).fill(' ');
+    this.track = new Array(this.trackLength).fill(" ");
     this.track[this.hero.position] = this.hero.skin;
     this.track[this.enemy.position] = this.enemy.skin;
+    this.track[this.boomerang.position] = this.boomerang.skin;
   }
 
   check() {
     if (this.hero.position === this.enemy.position) {
       this.hero.die();
+    } else if (this.boomerang.position === this.enemy.position) {
+      this.enemy.die();
+    } else if (this.boomerang.position === this.hero.position) {
+      console.log("YOU WIN!");
+      process.exit();
     }
   }
 
   play() {
     setInterval(() => {
       // Let's play!
+      if (this.enemy.position > this.boomerang.position) {
+        this.boomerang.moveRight();
+      } else {
+        this.boomerang.moveLeft();
+      }
+      this.enemy.moveLeft();
+
       this.check();
       this.regenerateTrack();
       this.view.render(this.track);
-    });
+    }, 300);
   }
 }
 
